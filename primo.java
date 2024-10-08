739a899438ac21c623255af8de7ea9792c207a50
Thalysztx <thalysson072007@gmail.com>
2024-10-07 22:14:33 -0300

    Add files via upload

---
+++ primo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class primo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int contador = 0;
+        //10 numeros int
+        System.out.println("Digite 10 numeros INTEIROS: ");
+        for (int i = 0; i < 10; i++){
+            numeros[i] = scanner.nextInt();
+        }
+        //num especifico pra contar
+        System.out.println("Valor especifico pra contar: ");
+        int numeroEspecificos = scanner.nextInt();
+        for (int i = 0; i < 10; i++){
+            if (numeros[i] == numeroEspecificos){
+                contador++;
+            }
+        }
+        //resultado
+        System.out.println("o numero " + numeroEspecificos + " aparece " + contador + " vezes ");
+        scanner.close();
+    }
+}
